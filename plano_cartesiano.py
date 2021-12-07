5b19385c0fdec4a20b4278721276715dc7e55bb5
Simeone-Holanda <simeone.10s10@gmail.com>
2021-12-07 18:06:37 -0300

    Criado a captura de pontos de um plano cartesiano na gui.

--- plano_cartesiano.py
+++ plano_cartesiano.py
@@ -11,11 +11,8 @@ master.title('Plano cartesiano')
 master.geometry("%dx%d+0+0" %(screen_width,screen_height))  # largura, altura, dist esquerda + dist topo
 master.wm_resizable(width=False, height=False)  # travando a tela na resolução definida
 
-# importando img
-# img = PhotoImage(file="plano_cartesiano.jpg")
 
 # colocando a img de fundo e especificando a posição dela na tela
-
 canvas = Canvas(master, width=1920, height=1080, bg="white")
 canvas.pack()
 label_plano_cartesiano = Label(master)
@@ -27,21 +24,48 @@ canvas.create_line(screen_width/2,0,screen_width/2,screen_height) #linha  vertic
 
 # criando a caixinha q mostra as coordenadas
 widget1 = Frame(canvas)
-widget1.place(bordermode=OUTSIDE, height=50, width=500, x=(screen_width/2)-250)
+widget1.place(bordermode=OUTSIDE, height=50, width=400, x=250)
 msg = Label(widget1, text=f"Coordenadas da tela X:{0} | Y: {0}")
 msg["font"] = ("Verdana", "10", "italic", "bold")
 msg.pack()
 
+coordenadas_plano_cartesiano = Frame(canvas)
+coordenadas_plano_cartesiano.place(bordermode=OUTSIDE, height=50, width=400, x=800)
+msg_plano = Label(coordenadas_plano_cartesiano, text=f"Coordenadas do plano X:{0} | Y: {0}")
+msg_plano["font"] = ("Verdana", "10", "italic", "bold")
+msg_plano.pack()
+
+def converter_plano_cartesiano(x,y):
+    ...
 
 def button(event):
-    """ Função responsável por mostrar as coordenadas na tela e no console. """
+    """ Função responsável por mostrar as coordenadas na tela e marca o pixel selecionado. """
 
     canvas.delete("pixelGroup")#Exclui caso exista o grupo de pixeis
     x = event.x
     y = event.y
-    msg.configure(text=f'X:{x} | Y: {y}')
+    if x <= screen_width/2:
+        print('x é negativo ou igual a 0!!')
+        if y <= screen_height/2:
+            print('y é positivo!!')
+            msg.configure(text=f'X:{x} | Y: {y}')
+            msg_plano.configure(text=f'X:{round(((screen_width/2)-x))*-1} | Y: {round(((screen_height/2)-y))}')
+        else:
+            print('y é negativo ou igual a 0 !!')
+            msg.configure(text=f'X:{x} | Y: {y}')
+            msg_plano.configure(text=f'X:{round(((screen_width/2)-x))*-1} | Y: {round((((screen_height/2)-y)))}')
+    else:
+        print('x é positivo!!')
+        if y < screen_height/2:
+            print('y é positivo!!')
+            msg.configure(text=f'X:{x} | Y: {y}')
+            msg_plano.configure(text=f'X:{round(((screen_width/2)-x))*-1} | Y: {round(((screen_height/2)-y))}')
+        else:
+            print('y é negativo!!')
+            msg.configure(text=f'X:{x} | Y: {y}')
+            msg_plano.configure(text=f'X:{round(((screen_width/2)-x))*-1} | Y: {round(((screen_height/2)-y))}')
+    
     print(f'X:{x} | Y: {y}')
-
     canvas.create_rectangle(x, y, x+10, y+10, fill="red",tags="pixelGroup") #gera grupo de pixeis
 
 
