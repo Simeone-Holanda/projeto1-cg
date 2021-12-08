6c8d4904434ff013d9f54388c07e5bdb661a43a6
MatheusOliveiraCode <58707195+MatheusOliveiraCode@users.noreply.github.com>
2021-12-08 17:14:00 -0300

    Tela Corrigida
    Tela Corrigida

--- plano_cartesiano.py
+++ plano_cartesiano.py
@@ -5,8 +5,11 @@ from transformacoes import Transformar
 master = Tk()
 
 # captura tamanho da tela do usuário
-screen_width = master.winfo_screenwidth()
-screen_height = master.winfo_screenheight()
+screen_width = 1000
+screen_height = 900
+
+canvasSizeX = 900
+canvasSizeY = 800
 
 master.title('Projeto 1: Transformacoes')
 
@@ -14,50 +17,45 @@ master.geometry("%dx%d+0+0" % (screen_width, screen_height))  # largura, altura,
 master.wm_resizable(width=False, height=False)  # travando a tela na resolução definida
 
 # colocando a img de fundo e especificando a posição dela na tela
-canvas = Canvas(master, width=1920, height=1080, bg="white")
-canvas.pack()
+canvas = Canvas(master, width=900, height=800, bg="white")
+canvas.pack(side="bottom")
 label_plano_cartesiano = Label(master)
 label_plano_cartesiano.place(x=0, y=0)
 
 # colocar o pixel como imagem
 img = PhotoImage(width=screen_width, height=screen_height)
-canvas.create_image((screen_width/2, screen_height/2), image=img, state="normal") #normal, disabled or hidden
+canvas.create_image((screen_width / 2, screen_height / 2), image=img, state="normal")  # normal, disabled or hidden
 
 # criando plano cartesiano
-canvas.create_line(0, screen_height / 2, screen_width, screen_height / 2)  # Linha horizontal
-canvas.create_line(screen_width / 2, 0, screen_width / 2, screen_height)  # linha  vertical
+canvas.create_line(0, 400, 900, 400)  # Linha horizontal
+canvas.create_line(450, 0, 450, 800)  # linha  vertical
 
 # criando a caixinha q mostra as coordenadas
-widget1 = Frame(canvas)
+widget1 = Frame(master)
 widget1.place(bordermode=OUTSIDE, height=20, width=300, x=50)
 msg = Label(widget1, text=f"Coordenadas da tela X:{0} | Y: {0}")
 msg["font"] = ("Verdana", "10", "italic", "bold")
 msg.pack()
 
-coordenadas_plano_cartesiano = Frame(canvas)
+coordenadas_plano_cartesiano = Frame(master)
 coordenadas_plano_cartesiano.place(bordermode=OUTSIDE, height=20, width=400, x=450)
 msg_plano = Label(coordenadas_plano_cartesiano, text=f"Coordenadas do plano X:{0} | Y: {0}")
 msg_plano["font"] = ("Verdana", "10", "italic", "bold")
 msg_plano.pack()
 
-coordenadas_ndc = Frame(canvas)
-coordenadas_ndc.place(bordermode=OUTSIDE, height=20, width=400, x=1100)
+coordenadas_ndc = Frame(master)
+coordenadas_ndc.place(bordermode=OUTSIDE, height=20, width=400, y=70,x=20)
 msg_ndc = Label(coordenadas_ndc, text=f"Coordenadas NDC NDCX:{0} | NDCY: {0}")
 msg_ndc["font"] = ("Verdana", "10", "italic", "bold")
-msg_ndc.pack()
+msg_ndc.pack(side="bottom")
 
-coordenadas_dc = Frame(canvas)
-coordenadas_dc.place(bordermode=OUTSIDE, height=20, width=300, x=1550)
+coordenadas_dc = Frame(master)
+coordenadas_dc.place(bordermode=OUTSIDE, height=20, width=300,x=500,y=70)
 msg_dc = Label(coordenadas_dc, text=f"Coordenadas DC DCX:{0} | DCY: {0}")
 msg_dc["font"] = ("Verdana", "10", "italic", "bold")
-msg_dc.pack()
+msg_dc.pack(side="bottom")
 
 
-
-
-
-def converter_plano_cartesiano(x, y):
-    ...
 
 
 def button(event):
@@ -65,39 +63,35 @@ def button(event):
 
     transformar = Transformar()
 
-
-
     x = event.x
     y = event.y
     msg.configure(text=f'Coordenadas da tela X:{x} | Y: {y}')
     msg_plano.configure(
-        text=f'Coordenadas do plano X:{round(x - (screen_width / 2) - 1) + 1} | Y: {(round(y - (screen_height / 2) - 1) * -1) - 1}')
+        text=f'Coordenadas do plano X:{round(x - (canvasSizeX / 2) - 1) + 1} | Y: {(round(y - (canvasSizeY / 2) - 1) * -1) - 1}')
     print(f'X:{x} | Y: {y}')
 
-    x_plano = round(x - (screen_width / 2) - 1) + 1
-    y_plano = (round(((y - screen_height / 2) - 1)) * -1) - 1
+    x_plano = round(x - (canvasSizeX / 2) - 1) + 1
+    y_plano = (round(((y - canvasSizeY / 2) - 1)) * -1) - 1
 
-    ndcx = round(transformar.world_to_ndcx(x_plano, (screen_width / 2) - 1), 4)
-    ndcy = round(transformar.world_to_ndcy(y_plano, (screen_height / 2) - 1), 4)
+    ndcx = round(transformar.world_to_ndcx(x_plano, (canvasSizeX / 2) - 1), 4)
+    ndcy = round(transformar.world_to_ndcy(y_plano, (canvasSizeY / 2) - 1), 4)
 
-    dcx = transformar.ndcx_to_dcx(ndcx, screen_width / 2)
-    dcy = transformar.ndcy_to_dcy(ndcy, screen_height / 2)
+    dcx = transformar.ndcx_to_dcx(ndcx, canvasSizeX / 2)
+    dcy = transformar.ndcy_to_dcy(ndcy, canvasSizeY / 2)
 
     msg_ndc.configure(text=f'Coordenadas NDC NDCX:{ndcx} | NDCY: {ndcy}')
 
     msg_dc.configure(text=f'Coordenadas DC DCX:{dcx} | DCY: {dcy}')
 
-    
-
-    data = (  #Definição do grupo de pixeis
-       ("red", "red", "red", "red", "red"),
-       ("red", "red", "red", "red", "red"),
-       ("red", "red", "red", "red", "red"),
-       ("red", "red", "red", "red", "red"),
-       ("red", "red", "red", "red", "red"),
+    data = (  # Definição do grupo de pixeis
+        ("red", "red", "red", "red", "red"),
+        ("red", "red", "red", "red", "red"),
+        ("red", "red", "red", "red", "red"),
+        ("red", "red", "red", "red", "red"),
+        ("red", "red", "red", "red", "red"),
     )
 
-    img.put(data, (x-3, y-3)) #Impressão do pixel
+    img.put(data, (x-2, y-2))  # Impressão do pixel
 
 
 master.bind('<Button>', button)
